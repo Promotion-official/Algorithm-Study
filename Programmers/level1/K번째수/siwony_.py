3cd7ac620b1c2bf4e6ecfab87daf723d37bfc1a5
siwony_ <siwon103305@gmail.com>
2021-02-12 21:07:27 +0900

    WebHook test

--- Programmers/level1/K번째수/siwony_.py
+++ Programmers/level1/K번째수/siwony_.py
@@ -4,5 +4,5 @@ def solution(arr, commands):
         sort_arr = sorted((arr[com[0] - 1 : com[1]]))
         answer.append(sort_arr[com[2] - 1])
     return answer
-###테스트 코드###
+####테스트 코드####
 print(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
