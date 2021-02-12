f09f5c64e6b2d43ecc9979992194648dd01cda91
siwony_ <siwon103305@gmail.com>
2021-02-12 18:46:14 +0900

    [UPDATE]수박수박 1line

--- Programmers/level1/수박수박수박수박수박수?/siwony_.py
+++ Programmers/level1/수박수박수박수박수박수?/siwony_.py
@@ -1,8 +1,5 @@
 def solution(n):
-    answer = ""
-    for i in (range(n)):
-        answer += "수" if i % 2 == 0 else "박"
-    return answer
+    return ("수박" * n * 1)[0:n]
 ###테스트코드###
 print(solution(4))
 print(solution(5))
