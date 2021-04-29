a0f687db06d1522b824972448ab904ac0f0bd15e
siwony_ <siwon103305@gmail.com>
2021-04-29 14:00:31 +0900

    [UPDATE] k 번쨰 수 refactoring code

--- Programmers/level1/K번째수/siwony_.cs
+++ Programmers/level1/K번째수/siwony_.cs
@@ -2,21 +2,27 @@ using System;
 
 public class Solution {
     public int[] solution(int[] array, int[,] commands) {
-          int[] answer = new int[commands.GetLength(0)];
+        
+        int commandsTwoDimensionalLen = commands.GetLength(0);
+        int[] answer = new int[commandsTwoDimensionalLen];
 
-            for (int i = 0; i < commands.GetLength(0); i++){
-                int[] arraySort = new int[commands[i, 1] - commands[i, 0] + 1];
-                int arraySortIdx = 0;
-                for (int j = commands[i, 0]; j <= commands[i, 1]; j++){
-                    arraySort[arraySortIdx++] = array[j - 1];
+        for (int i = 0; i < commandsTwoDimensionalLen; i++){
+            int startNum = commands[i, 0];
+            int endNum = commands[i, 1];
+            int pickNum = commands[i, 2] - 1;
+            
+            int[] arraySort = new int[endNum - startNum + 1];
+            int arraySortIdx = 0;
+            for (int j = startNum; j <= endNum; j++){
+                arraySort[arraySortIdx++] = array[j - 1];
 
-                    if (j == commands[i, 1]){
-                        Array.Sort(arraySort, (a, b) => a > b ? 1 : -1);
-                        answer[i] = arraySort[commands[i, 2] - 1];
-                    }
+                if (j == commands[i, 1]){
+                    Array.Sort(arraySort, (a, b) => a > b ? 1 : -1);
+                    answer[i] = arraySort[pickNum];
                 }
             }
+        }
 
-            return answer;
+        return answer;
     }
 }
